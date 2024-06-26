6e9db13ca50c75f1cdd501b72418a71710d6775f
kifle23 <kiflekifle23@gmail.com>
2024-06-26 15:38:45 +0300

    Add funders to faucet contract

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -2,6 +2,16 @@
 pragma solidity ^0.8.13;
 
 contract Faucet {
+
+    address[] public funders;
+
     receive() external payable {}
-    function addFunds() external payable {}
+    
+    function addFunds() external payable {
+        funders.push(msg.sender);
+    }
+
+    function getAllFunders() external view returns (address[] memory) {
+        return funders;
+    }   
 }
