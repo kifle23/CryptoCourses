212e008346192b959a22fa98b42cf9dd8e89535b
kifle23 <kiflekifle23@gmail.com>
2024-06-26 18:11:33 +0300

    Avoid duplicate addresses

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,25 +3,26 @@ pragma solidity ^0.8.13;
 
 contract Faucet {
     uint public numOfFunders;
-    mapping(uint => address) private funders;
+    mapping(address => bool) private funders;
+    address[] private funderList;
 
     receive() external payable {}
-    
+
     function addFunds() external payable {
-        numOfFunders++;
-        funders[numOfFunders] = msg.sender;
+        address funder = msg.sender;
+        if (!funders[funder]) {
+            numOfFunders++;
+            funders[funder] = true;
+            funderList.push(funder);
+        }
     }
 
     function getAllFunders() external view returns (address[] memory) {
-        address[] memory allFunders = new address[](numOfFunders);
-        for (uint i = 1; i <= numOfFunders; i++) {
-            allFunders[i - 1] = funders[i];
-        }
-        return allFunders;
-    } 
+        return funderList;
+    }
 
     function getFunderAtIndex(uint index) external view returns (address) {
         require(index < numOfFunders, "Index out of bounds");
-        return funders[index+1];
-    }  
+        return funderList[index];
+    }
 }
