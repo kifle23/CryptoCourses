1fe115b98094ea6535894bb6a4b998b65ec3855b
kifle23 <kiflekifle23@gmail.com>
2024-06-27 11:40:49 +0300

    Get accounts

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,8 @@ function App() {
     provider: null,
     web3: null,
   });
-  
+  const [account, setAccount] = useState<string>();
+
   useEffect(() => {
     const loadProvider = async () => {
       let provider = null;
@@ -22,32 +23,44 @@ function App() {
           await provider.request({ method: "eth_requestAccounts" });
         } catch (error) {
           console.error("User denied account access");
-        }       
+        }
       } else if ((window as any).web3) {
         provider = (window as any).web3.currentProvider;
-      } else if (process.env.NODE_ENV !== 'production') {
+      } else if (process.env.NODE_ENV !== "production") {
         provider = new Web3.providers.HttpProvider("http://localhost:7545");
       }
-      
+
       if (provider) {
         setWeb3Api({
           provider,
-          web3: new Web3(provider)
+          web3: new Web3(provider),
         });
       }
     };
     loadProvider();
   }, []);
 
-  console.log("Web3api: ",web3Api.web3);
+  useEffect(() => {
+    const getAccounts = async () => {
+      if (web3Api.web3) {
+        const accounts = await web3Api.web3.eth.getAccounts();
+        setAccount(accounts[0]);
+      }
+    };
+    web3Api.web3 && getAccounts();
+  }, [web3Api.web3]);
 
   return (
     <div className="faucet-wrapper">
       <div className="faucet">
+        <span>
+          <strong>Connected Account:</strong>{" "}
+          {account ? account : "No account connected"}
+        </span>
         <div className="balance-view is-size-2">
           Current Balance: <strong>10</strong>ETH
         </div>
-        
+
         <button className="button mr-2">Donate</button>
         <button className="button">Withdraw</button>
       </div>
