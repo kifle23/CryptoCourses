462d53c218654eb389dddd39f2d4a8d691b053a4
kifle23 <kiflekifle23@gmail.com>
2024-06-28 16:12:38 +0300

    Refactor actions logic from app.tsx

---
+++ src/components/Actions/Actions.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Actions: React.FC = () => {
+  return (
+    <>
+      <button className="button mr-2 is-link">Donate</button>
+      <button className="button is-primary">Withdraw</button>
+    </>
+  );
+};
+
+export default Actions;
+
